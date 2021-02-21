db5ca0c050191e6b2c1bb35409241ae14a4ee078
Elise <50882635+elisecho@users.noreply.github.com>
2021-02-21 07:43:02 -0500

    Final update

--- app.py
+++ app.py
@@ -15,16 +15,37 @@ def welcome():
     return render_template('welcome.html')  # render a template
 
 
+@app.route('/submit')
+def submit():
+    return render_template('submit.html')  # render a template
+
+
+@app.route('/checkuser')
+def checkuser():
+    return render_template('checkuser.html')  # render a template
+
+
 # Route for handling the login page logic
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'patient' or request.form['password'] != 'p123':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('welcome'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/provider_login', methods=['GET', 'POST'])
+def provider():
     error = None
     if request.method == 'POST':
         if request.form['username'] != 'admin' or request.form['password'] != 'test123':
             error = 'Invalid Credentials. Please try again.'
         else:
             return redirect(url_for('portal'))
-    return render_template('login.html', error=error)
+    return render_template('provider_login.html', error=error)
 
 
 # Route to vaccination signup page
@@ -42,8 +63,7 @@ def signup():
         }
         all_signups.append(new_signup)
         # Once form is submitted send the user back to the homepage
-        # TODO Change this to another redirect (Like a page that displays "Form has been submitted!")
-        return redirect(url_for('portal'))
+        return redirect(url_for('submit'))
     return render_template('signup.html')
 
 
@@ -56,7 +76,6 @@ def portal():
 # start the server with the 'run()' method
 if __name__ == '__main__':
     app.run(debug=True)
-
 
 # To run app, in terminal type:
 # set FLASK_APP=app.py
